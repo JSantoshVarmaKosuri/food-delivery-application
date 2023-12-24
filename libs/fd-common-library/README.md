d02edbda59107a702baf742aafbe8f04dc023242
J Santosh Varma Kosuri <dev.santoshvarma@gmail.com>
2023-12-24 14:34:16 +0530

    NX workspace init setup for gateway,services and libraries.

---
+++ libs/fd-common-library/README.md
@@ -0,0 +1,7 @@
+# fd-common-library
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test fd-common-library` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/fd-common-library/src/lib/fd-common-library.spec.ts
@@ -0,0 +1,7 @@
+import { fdCommonLibrary } from './fd-common-library';
+
+describe('fdCommonLibrary', () => {
+  it('should work', () => {
+    expect(fdCommonLibrary()).toEqual('fd-common-library');
+  });
+});

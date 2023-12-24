6117f709b18374e1f299eb4d3793155644d04bba
J Santosh Varma Kosuri <dev.santoshvarma@gmail.com>
2023-12-25 01:02:10 +0530

    Added Domain Events, with priliminary kafka listeners and publishers.

--- services/fd-order-service/src/MesageQueue/OrderPublisher.ts
+++ services/fd-order-service/src/MesageQueue/OrderPublisher.ts
@@ -1,5 +1,4 @@
 import { DomainEvent, DomainPublisherEvent } from "@food-delivery-application/fd-common-library";
-import { OrderApprovedEvent } from "../DomainCore/Events/OrderApprovedEvent";
 import { Order } from "../DomainCore/Entities/Order";
 import { Producer } from "kafkajs";
 
@@ -9,13 +8,13 @@ export class OrderPublisher extends DomainPublisherEvent<Order, DomainEvent<Orde
     super(producer);
   }
 
-  async publish(event: OrderApprovedEvent) {
+  async publish(event: DomainEvent<Order>) {
     await this.producer.connect()
     await this.producer.send({
       topic: event.topic,
       messages: [
         { value: JSON.stringify(event.data) },
-      ],
+      ]
     })
 
     await this.producer.disconnect()

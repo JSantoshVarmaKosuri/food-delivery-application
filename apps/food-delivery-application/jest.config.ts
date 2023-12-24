aece611b06119d897028db823696936b3a755a70
J Santosh Varma Kosuri <dev.santoshvarma@gmail.com>
2023-12-24 14:18:43 +0530

    Initial commit

---
+++ apps/food-delivery-application/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'food-delivery-application',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/food-delivery-application',
+};
